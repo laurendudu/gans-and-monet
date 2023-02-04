52d1d3c6717c07ee4d4c6d0439e77d208f80238f
Lauren Durivault <60437222+laurendudu@users.noreply.github.com>
2023-02-04 16:57:50 +0100

    Diffaugment + cyclegan (#4)

--- utils/preprocessing.py
+++ utils/preprocessing.py
@@ -29,3 +29,58 @@ def load_dataset(filenames, batch_size):
     dataset = dataset.map(tfrecord_to_image, num_parallel_calls=AUTOTUNE)
     dataset = dataset.batch(batch_size)
     return dataset
+
+
+def load_dataset(filenames):
+    dataset = tf.data.TFRecordDataset(filenames)
+    dataset = dataset.map(tfrecord_to_image, num_parallel_calls=AUTOTUNE)
+    return dataset
+
+
+def get_gan_dataset(
+    monet_files, photo_files, augment=None, repeat=True, shuffle=True, batch_size=1
+):
+
+    monet_ds = load_dataset(monet_files)
+    photo_ds = load_dataset(photo_files)
+
+    if repeat:
+        monet_ds = monet_ds.repeat()
+        photo_ds = photo_ds.repeat()
+    if shuffle:
+        monet_ds = monet_ds.shuffle(2048)
+        photo_ds = photo_ds.shuffle(2048)
+
+    monet_ds = monet_ds.batch(batch_size, drop_remainder=True)
+    photo_ds = photo_ds.batch(batch_size, drop_remainder=True)
+    if augment:
+        monet_ds = monet_ds.map(augment, num_parallel_calls=AUTOTUNE)
+        photo_ds = photo_ds.map(augment, num_parallel_calls=AUTOTUNE)
+
+    monet_ds = monet_ds.prefetch(AUTOTUNE)
+    photo_ds = photo_ds.prefetch(AUTOTUNE)
+
+    gan_ds = tf.data.Dataset.zip((monet_ds, photo_ds))
+
+    return gan_ds
+
+
+def get_photo_dataset(
+    photo_files, augment=None, repeat=False, shuffle=False, batch_size=1
+):
+
+    photo_ds = load_dataset(photo_files)
+
+    if repeat:
+        photo_ds = photo_ds.repeat()
+    if shuffle:
+        photo_ds = photo_ds.shuffle(2048)
+
+    photo_ds = photo_ds.batch(batch_size, drop_remainder=True)
+
+    if augment:
+        photo_ds = photo_ds.map(augment, num_parallel_calls=AUTOTUNE)
+
+    photo_ds = photo_ds.prefetch(AUTOTUNE)
+
+    return photo_ds
